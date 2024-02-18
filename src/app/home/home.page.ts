6d64e456f565666513664f4e48088f02615eb1db
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-02-18 18:34:34 +0700

    test api call on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,15 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private movieService = inject(MovieService);
+
+  constructor() {
+    this.loadMovies();
+  }
+
+  loadMovies() {
+    this.movieService.getTopRatedMovies().subscribe((movies) => {
+      console.log(movies);
+    })
+  }
 }
