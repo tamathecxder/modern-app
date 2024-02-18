5b4a54aa1e512c4a819760db1de46d6b284d6d7b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-02-18 18:21:57 +0700

    create 2 method for getting top rated & movie details

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { API_KEY, BASE_URL } from '../constants/global';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private http = inject(HttpClient);
 
   constructor() { }
+
+  getTopRatedMovies(page:number = 1) {
+    return this.http.get(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
+  }
+
+  getMovieDetails(id: string) {
+    return this.http.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  }
 }
