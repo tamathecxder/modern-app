86b9d0dc5023cc772305224eaa7f2f5307a0016f
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-02-18 18:41:00 +0700

    declare related type on movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { API_KEY, BASE_URL } from '../constants/global';
+import { Observable } from 'rxjs';
+import { ApiResult, MovieResult } from './interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,11 @@ export class MovieService {
 
   constructor() { }
 
-  getTopRatedMovies(page:number = 1) {
-    return this.http.get(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
+  getTopRatedMovies(page:number = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`);
   }
 
-  getMovieDetails(id: string) {
-    return this.http.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  getMovieDetails(id: string): Observable<MovieResult> {
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
   }
 }
